c46a37045565c0ed993a4ba6bdab28c2d13970a3
Chandrika Koppad <chandrikakoppad@gmail.com>
2026-07-21 15:14:32 +0530

    Time: 243 ms (14.77%), Space: 13 MB (45.83%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        new_str = ""
+        for ch in s:
+            if ch.isalnum():
+                new_str += ch.lower()
+        return new_str == new_str[::-1]
+        
