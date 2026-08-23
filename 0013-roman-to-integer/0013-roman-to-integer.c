7d609addb445ceeb5a90c4d94a7a134a153a1e1d
Chandrika Koppad <chandrikakoppad@gmail.com>
2026-08-23 19:51:57 +0530

    Time: 3 ms (18.9%), Space: 12.4 MB (6.86%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,29 +1,21 @@
-#include <stdio.h>
+int romanToInt(char* s) {
+    int value[256] = {0};
 
-int value(char c) {
-    switch (c) {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
-        default: return 0;
-    }
-}
+    value['I'] = 1;
+    value['V'] = 5;
+    value['X'] = 10;
+    value['L'] = 50;
+    value['C'] = 100;
+    value['D'] = 500;
+    value['M'] = 1000;
 
-int romanToInt(char *s) {
     int result = 0;
 
     for (int i = 0; s[i] != '\0'; i++) {
-        int current = value(s[i]);
-        int next = value(s[i + 1]);
-
-        if (current < next) {
-            result -= current;
+        if (value[s[i]] < value[s[i + 1]]) {
+            result -= value[s[i]];
         } else {
-            result += current;
+            result += value[s[i]];
         }
     }
 
